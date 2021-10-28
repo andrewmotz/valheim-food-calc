8f54bdf71e96d5dd2cab2d3b91c43c3522a152ab
Andrew Motz <andrewmozza555@gmail.com>
2021-10-27 21:55:17 -0700

    Added basic bars around the food icon
    Added health and stamina bar around the food icon. Still lots to do on it.

--- src/FoodItem.js
+++ src/FoodItem.js
@@ -1,5 +1,35 @@
 import React from "react";
 import styles from './FoodIcon.module.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+// import StyledProgressbar from "./StyledProgressar";
+
+const HEALTHSTYLES = {
+    background: {
+      fill: "#3e98c7"
+    },
+    text: {
+      fill: "#FF0"
+    },
+    path: {
+      stroke: "#FF0000",
+      strokeLinecap: "BUTT"
+    },
+    trail: { stroke: "transparent" }
+  }
+
+  const STAMINASTLYES = {
+    background: {
+      fill: "#3e98c7"
+    },
+    text: {
+      fill: "#FF0"
+    },
+    path: {
+      stroke: "#EBFF19",
+      strokeLinecap: 'butt'
+    },
+    trail: { stroke: "transparent" }
+  }
 
 
 export default function FoodItem(jsonData, itemName){
@@ -11,6 +41,12 @@ export default function FoodItem(jsonData, itemName){
     return (
         <div className={styles.foodIcon}>
             <p className= {styles.itemName}>{foodObject.Name}</p>
+            <div className={styles.progressBars}>
+                <CircularProgressbar value = {foodObject.HP} maxValue={100} minValue={0} 
+                    counterClockwise={true} circleRatio={.5} styles={HEALTHSTYLES}/>
+                {/* <StyledProgressbar value = {5} maxValue={100} minValue={0} counterClockwise={true} circleRatio={.5}/> */}
+                <CircularProgressbar value = {foodObject.STAM} maxValue={50} circleRatio={.5} styles= {STAMINASTLYES}/>
+            </div>
             <img src={foodObject.FileName} alt= {foodObject.Name}/>
             <div className = {styles.stats}>
                 <p className={styles.health}>{foodObject.HP}</p>
