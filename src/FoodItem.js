e38ba7f6403f30f01e5d248ffb4cb97816b3de38
Andrew Motz <andrewmozza555@gmail.com>
2021-10-27 21:11:57 -0700

    Created super basic styling for food icon
    Created, linked and setup a basic stylesheet for the food icons

--- src/FoodItem.js
+++ src/FoodItem.js
@@ -5,15 +5,18 @@ import styles from './FoodIcon.module.css';
 export default function FoodItem(jsonData, itemName){
     console.log(jsonData);
 
+    //Gets the object from the JSON file based off the name
     let foodObject = jsonData[itemName];
     
     return (
         <div className={styles.foodIcon}>
-            <p>Name: {foodObject.Name}</p>
+            <p className= {styles.itemName}>{foodObject.Name}</p>
             <img src={foodObject.FileName} alt= {foodObject.Name}/>
-            <p>Health: {foodObject.HP}</p>
-            <p>Sum: { foodObject.HP + foodObject.STAM} </p>
-            <p>Stamina: {foodObject.STAM}</p>
+            <div className = {styles.stats}>
+                <p className={styles.health}>{foodObject.HP}</p>
+                <p className={styles.healthStamina}>{foodObject.HP + foodObject.STAM} </p>
+                <p className={styles.stamina}>{foodObject.STAM}</p>
+            </div>
         </div>
     )
 }
